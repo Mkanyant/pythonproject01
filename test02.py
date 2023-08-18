244b797d80fea7d66e9c396826518c0d3a6df77c
Mkanyant <S6519410049@sau.ac.th>
2023-08-18 11:56:28 +0700

    code v.1 : creat 3 file

---
+++ test02.py
@@ -0,0 +1,7 @@
+print("hello..." , end="")   #String
+print("Hey..." , end="")     #String
+print(1145335)      #Number
+print(True)         #boolean
+print("False")      #String
+print("54654515")   #String
+print(10+10+20)     #Expression 
